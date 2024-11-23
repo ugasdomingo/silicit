552ca9e17b7bb026afaf8fb994d0062b804b0238
unknown <ugasdomingo@gmail.com>
2024-11-23 19:08:07 +0100

    v1.1 F.O. adding videos to jobs

--- app/src/static/jobs.ts
+++ app/src/static/jobs.ts
@@ -9,9 +9,9 @@ export const jobs = [
             'Aprende en 4 semanas a crear páginas web con WordPress. No necesitas experiencia previa.',
         requirements: 'Acceso a una computadora con conexión a internet.',
         video_small:
-            '<iframe width="196" height="110.25" src="https://www.youtube-nocookie.com/embed/x4PU6pGEDWw?si=BCXxMNGNzkgbXf0t" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+            '<iframe width="196" height="110.25" src="https://www.youtube.com/embed/VFW4yTtKxjo?si=h3o822UrJAQz2dH4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
         video_large:
-            '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/x4PU6pGEDWw?si=BCXxMNGNzkgbXf0t" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+            '<iframe width="560" height="315" src="https://www.youtube.com/embed/VFW4yTtKxjo?si=h3o822UrJAQz2dH4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
         price: 50,
         start_date: '11/01/2025',
         quorum: 15,
@@ -29,9 +29,9 @@ export const jobs = [
             'Aprende en 3 semanas a manejar redes sociales con fines comerciales. No necesitas experiencia previa.',
         requirements: 'Acceso a internet.',
         video_small:
-            '<iframe width="196" height="110.25" src="https://www.youtube-nocookie.com/embed/x4PU6pGEDWw?si=BCXxMNGNzkgbXf0t" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+            '<iframe width="196" height="110.25" src="https://www.youtube.com/embed/dj_y9qUNloI?si=v7D9BRzF_RiUMS84" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
         video_large:
-            '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/x4PU6pGEDWw?si=BCXxMNGNzkgbXf0t" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+            '<iframe width="560" height="315" src="https://www.youtube.com/embed/dj_y9qUNloI?si=v7D9BRzF_RiUMS84" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
         price: 40,
         start_date: '11/01/2025',
         quorum: 15,
